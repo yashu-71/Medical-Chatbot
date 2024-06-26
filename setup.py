5b363fb2b41e45fe486e406fc4a1b56481677510
ShashiPreetham18 <shashipreetham1804@gmail.com>
2024-06-26 22:04:46 +0530

    all files are added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Medical Bot',
+    version='0.0.0',
+    author='Shashi',
+    author_email='whyforu00@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
